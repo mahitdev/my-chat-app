88d670c9694a52a68a009312876d67c99affae67
Mahit <mahitsaxena44@gmail.com>
2025-11-23 23:15:00 +0530

    Point frontend to Render server

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://my-chat-backend-zwfa.onrender.com",
   withCredentials: true,
 });
